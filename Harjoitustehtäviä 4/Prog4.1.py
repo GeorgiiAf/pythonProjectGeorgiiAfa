7dfb2313167473eb934c9640851f5fbbe78db144
GeorgiiAf <gosha3484@gmail.com>
2024-01-18 23:26:42 +0300

    tehtäviä 4

---
+++ Harjoitustehtäviä 4/Prog4.1.py
@@ -0,0 +1,5 @@
+a=0
+while a<=1000:
+    a=a+1
+    if a%3==0:
+        print(a)
---
+++ Harjoitustehtäviä 4/Prog4.2.py
@@ -0,0 +1,6 @@
+print('Syötä tuuman määrä ')
+a=float(input())
+while a>=0:
+    a=a*2.54
+    print('Vaadittu tuumamäärä senttimetreinä ', a,' cm')
+    a=float(input())
